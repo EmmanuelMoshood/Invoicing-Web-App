c201089be65e957a71967ec0463a93b1a1923692
Maye Jesuorobo <nayawes@yahoo.com>
2023-03-21 22:53:18 +0100

    made a simple function to only get the invoice page so I can build the ejs file

---
+++ controllers/invoices.js
@@ -0,0 +1,91 @@
+// const NewItem = require("../models/NewItems")
+
+export const getInvoices = (req, res) => {
+    res.render("invoices.ejs");
+  }
+/*
+  export const getInvoices = async (req, res) => {
+      try {
+          const items = await NewItem.find()
+          
+          let daysLeft = []
+          for(let i = 0; i < items.length; i++) {
+              let expirations = new Date(items[i].expiry)
+              let today = new Date();
+              let timeDiff = expirations.getTime() - today.getTime();
+              if (timeDiff <= 0) {
+                  daysLeft.push('Expired')
+                } else {
+                    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+                    daysLeft.push(diffDays)
+                }
+            }
+            console.log(daysLeft);
+            res.render('dashboard.ejs', {newItem: items, daysLeft: daysLeft})
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+        }
+    }
+    */
+   
+    /*
+    createItem: async (req, res) => {
+        let expiration = new Date(req.body.expiry);
+
+        const newItem = new NewItem(
+            {
+                itemInput: req.body.itemInput,
+                quantity: req.body.quantity,
+                unit: req.body.unit,
+                expiry: expiration
+            }
+        )
+        try {
+            await newItem.save()
+            console.log(newItem)
+            res.redirect('/dash')
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+            res.redirect('/dash')
+        }
+    },
+      // getting the edit page
+    editItems: async (req, res) => {
+        const id = req.params.id
+        console.log(id);
+        try {
+            const items = await NewItem.find()
+            res.render('editDashboard.ejs', { dashItems : items, dashId : id})
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+        }
+    },
+    // the actual update
+    updateItem: async (req, res) => {
+        const id = req.params.id
+        let expiration = new Date(req.body.expiry);
+        try {
+            console.log('Item has been updated');
+            await NewItem.findByIdAndUpdate(id, {
+                itemInput: req.body.itemInput,
+                quantity: req.body.quantity,
+                unit: req.body.unit,
+                expiry: expiration
+            })
+            res.redirect('/dash')
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+            res.redirect('/dash')
+        }
+    },
+    deleteItem: async (req, res) => {
+        const id = req.params.id
+        try {
+            const item = await NewItem.findByIdAndDelete(id)
+            console.log(item);
+            res.redirect('/dash')
+        } catch (err) {
+            if (err) return res.status(500).send(err)
+        }
+    }
+    */
